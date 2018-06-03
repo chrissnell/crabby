1f8da227fa2f777bdd1975efe6e264fd398895cc
Chris Snell <chris.snell@gmail.com>
2018-06-03 17:15:55 -0500

    Vendor deps with dep

---
+++ vendor/sourcegraph.com/sourcegraph/go-selenium/run-tests.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Run the test suite
+
+jar=$(./selenium.sh jar)
+if [ ! -f $jar ]; then
+	echo "error: can't find ${jar} (use './selenium.sh download' to get it)"
+	exit 1
+fi
+
+./selenium.sh start
+# Wait for selenium to start
+max_wait=20
+start_time=$(date +%s)
+while true;
+do
+	now=$(date +%s)
+	wait_time=$((now - start_time))
+	curl -s http://127.0.0.1:4444/wd/hub > /dev/null
+	if [ $? -eq 0 ]; then
+		echo "selenium server started after ${wait_time} seconds"
+		break
+	fi
+	if [ $wait_time -gt $max_wait ]; then
+		echo "error: selenium server didn't start after ${max_wait} seconds"
+		./selenium.sh stop
+		exit 1
+	fi
+done
+
+GOPATH=$(dirname $(dirname $PWD)) go test -v $@ selenium
+value=$!
+./selenium.sh stop
+exit $value
